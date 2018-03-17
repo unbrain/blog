1b9dced86b50e028351586297b8f6bebf2284f61
unbrain <marsorsun@gmail.com>
2018-03-17 20:04:53 +0800

    添加下拉菜单

--- task11/main.js
+++ task11/main.js
@@ -1,5 +1,6 @@
 let topNav = document.getElementById('topNav')
-let loadPage = document.getElementById('loadPage')
+let loadPage = document.getElementById('loading')
+let aMeauTris = document.getElementsByClassName('meauTrigger')
 document.onscroll = function (e) {
     if(window.scrollY !== 0){
         topNav.classList.add('active')
@@ -8,6 +9,17 @@ document.onscroll = function (e) {
     }
 }
 
-setTimeout(() => {
+setTimeout(function(){
     loadPage.classList.remove('active')
 }, 3000);
+
+for(let i = 0; i < aMeauTris.length; i++){
+    aMeauTris[i].onmouseenter = function (e) {
+        let c = e.currentTarget
+        c.classList.add('active')
+    }
+    aMeauTris[i].onmouseleave = function (e) {
+        let c = e.currentTarget
+        c.classList.remove('active')
+    }   
+}
