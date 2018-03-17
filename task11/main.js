0db238e04c3953c09c88a72b58c7ad74514d03df
unbrain <marsorsun@gmail.com>
2018-03-17 21:07:00 +0800

    添加滑动

--- task11/main.js
+++ task11/main.js
@@ -1,6 +1,7 @@
 let topNav = document.getElementById('topNav')
 let loadPage = document.getElementById('loading')
 let aMeauTris = document.getElementsByClassName('meauTrigger')
+let aMenus= document.querySelectorAll('.clearfix > li > a')
 document.onscroll = function (e) {
     if(window.scrollY !== 0){
         topNav.classList.add('active')
@@ -22,4 +23,14 @@ for(let i = 0; i < aMeauTris.length; i++){
         let c = e.currentTarget
         c.classList.remove('active')
     }   
+}
+for(let i = 0; i < aMenus.length; i++){
+    aMenus[i].onclick = function (e) {
+        e.preventDefault()
+        let a = e.currentTarget
+        let href = a.getAttribute('href')
+        let element = document.querySelector(href)
+        let top = element.offsetTop
+        window.scrollTo(0, top - 70)
+    }
 }
