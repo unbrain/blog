091572caed274af7d086583516d808f20d15e291
unbrain <marsorsun@gmail.com>
2018-03-18 17:26:06 +0800

    添加导航栏自动高亮

--- Resume/main.js
+++ Resume/main.js
@@ -2,17 +2,24 @@ let topNav = document.getElementById('topNav')
 let loadPage = document.getElementById('loading')
 let aMeauTris = document.getElementsByClassName('meauTrigger')
 let aMenus= document.querySelectorAll('.clearfix > li > a')
+let closeTags = document.querySelectorAll('[data-y]')
+
+for(let i = 0; i < closeTags.length; i++){
+    closeTags[i].classList.add('offset')
+}
+findClosest()
 document.onscroll = function (e) {
     if(window.scrollY !== 0){
         topNav.classList.add('active')
     }else{
         topNav.classList.remove('active')
     }
+    findClosest()
 }
 
-setTimeout(function(){
-    loadPage.classList.remove('active')
-}, 3000);
+// setTimeout(function(){
+//     loadPage.classList.remove('active')
+// }, 0);
 
 for(let i = 0; i < aMeauTris.length; i++){
     aMeauTris[i].onmouseenter = function (e) {
@@ -30,7 +37,40 @@ for(let i = 0; i < aMenus.length; i++){
         let a = e.currentTarget
         let href = a.getAttribute('href')
         let element = document.querySelector(href)
-        let top = element.offsetTop
-        window.scrollTo(0, top - 70)
+        let top = element.offsetTop - 70
+
+        function animate(time) {
+            requestAnimationFrame(animate);
+            TWEEN.update(time);
+        }
+        requestAnimationFrame(animate);
+
+        var coords = { x: window.scrollY}; // Start at (0, 0)
+        var tween = new TWEEN.Tween(coords) // Create a new tween that modifies 'coords'.
+            .to({ x: top}, 1000) // Move to (300, 200) in 1 second.
+            .easing(TWEEN.Easing.Quadratic.InOut) // Use an easing function to make the animation smooth.
+            .onUpdate(function () { // Called after tween.js updates 'coords'.
+                // Move 'box' to the position described by 'coords' with a CSS translation.
+                window.scrollTo(0, coords.x)
+            })
+            .start(); // Start the tween immediately.
     }
+}
+
+function findClosest() {
+    let minIndex = 0
+    for(let i = 0; i < closeTags.length; i++){           
+        if (Math.abs(closeTags[i].offsetTop - window.scrollY) < (Math.abs(closeTags[minIndex].offsetTop - window.scrollY))){
+            minIndex = i
+        }    
+    }
+    id = closeTags[minIndex].id
+    closeTags[minIndex].classList.remove('offset')
+    let a = document.querySelector('a[href="#'+ id +'"]')
+    let li = a.parentNode
+    let brothers = li.parentNode.children
+    for(let i = 0; i < brothers.length; i++){
+        brothers[i].classList.remove('heightLight')
+    }
+    li.classList.add('heightLight')
 }
