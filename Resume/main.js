cfa2e849626bd6e8560eeea01e68199ddc6504bb
unbrain <marsorsun@gmail.com>
2018-04-09 20:50:52 +0800

    修改css

--- Resume/main.js
+++ Resume/main.js
@@ -74,3 +74,20 @@ function findClosest() {
     }
     li.classList.add('heightLight')
 }
+
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
