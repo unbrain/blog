5bfbab617abcf86a3501677075c029fac4bc6831
unbrain <marsorsun@gmail.com>
2018-04-12 21:06:15 +0800

    使用mvc中的view & controller

--- Resume/js/auto-side-up.js
+++ Resume/js/auto-side-up.js
@@ -1,28 +1,30 @@
-let closeTags = document.querySelectorAll('[data-y]')
+!function(){
+    let closeTags = document.querySelectorAll('[data-y]')
 
-for (let i = 0; i < closeTags.length; i++) {
-    closeTags[i].classList.add('offset')
-}
-
-findClosest()
-document.addEventListener('scroll', () => {
-    findClosest()
-}) 
-
-function findClosest() {
-    let minIndex = 0
     for (let i = 0; i < closeTags.length; i++) {
-        if (Math.abs(closeTags[i].offsetTop - window.scrollY) < (Math.abs(closeTags[minIndex].offsetTop - window.scrollY))) {
-            minIndex = i
+        closeTags[i].classList.add('offset')
+    }
+
+    findClosest()
+    document.addEventListener('scroll', () => {
+        findClosest()
+    })
+
+    function findClosest() {
+        let minIndex = 0
+        for (let i = 0; i < closeTags.length; i++) {
+            if (Math.abs(closeTags[i].offsetTop - window.scrollY) < (Math.abs(closeTags[minIndex].offsetTop - window.scrollY))) {
+                minIndex = i
+            }
         }
+        id = closeTags[minIndex].id
+        closeTags[minIndex].classList.remove('offset')
+        let a = document.querySelector('a[href="#' + id + '"]')
+        let li = a.parentNode
+        let brothers = li.parentNode.children
+        for (let i = 0; i < brothers.length; i++) {
+            brothers[i].classList.remove('heightLight')
+        }
+        li.classList.add('heightLight')
     }
-    id = closeTags[minIndex].id
-    closeTags[minIndex].classList.remove('offset')
-    let a = document.querySelector('a[href="#' + id + '"]')
-    let li = a.parentNode
-    let brothers = li.parentNode.children
-    for (let i = 0; i < brothers.length; i++) {
-        brothers[i].classList.remove('heightLight')
-    }
-    li.classList.add('heightLight')
-}
+}.call()
--- Resume/js/topNavBar-sticky.js
+++ Resume/js/topNavBar-sticky.js
@@ -1,9 +1,27 @@
-let topNav = document.getElementById('topNav')
-
-document.addEventListener('scroll', (e) => {
-    if (window.scrollY !== 0) {
-        topNav.classList.add('active')
-    } else {
-        topNav.classList.remove('active')
+!function(){
+    let view = document.querySelector('#topNav')   
+    let controller = {
+        view: null,
+        init: function(view) {
+            this.view = view
+            this.bindEvents()
+        },
+        bindEvents: function () {
+            let view = this.view
+            document.addEventListener('scroll', (e) => {
+                if (window.scrollY !== 0) {
+                    this.active()
+                } else {
+                    this.deactive()
+                }
+            })
+        },
+        active:function(){
+            this.view.classList.add('active')
+        },
+        deactive:function(){
+            this.view.classList.remove('active')
+        },       
     }
-}) 
+    controller.init(view)
+}.call()
